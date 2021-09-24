b1bf60ee49756539879c4d165b0b36f251954c74
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 <fg.guzman@uniandes.edu.co>
2021-09-24 14:20:13 -0500

    primer commit del ejercicio

---
+++ main.js
@@ -0,0 +1,14 @@
+const URL = "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json";
+fetch(URL).then(res=>res.json().then(res=>{
+    let data = res;
+    let table = document.getElementById("table");
+    let i = 1;
+    data.forEach(element => {
+        let tr = document.createElement('tr');
+        tr.innerHTML = '<td>' + i + '</td>' +
+        '<td>' + element.events + '</td>' +
+        '<td>' + element.squirrel + '</td>';
+        table.appendChild(tr);
+        i++;
+    });
+}))
